61f163e30b9be6de2e512c2f1ad7cae632f9af22
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-02-01 19:44:21 +0530

    insertion complete

--- linkedlist/insertion1.cpp
+++ linkedlist/insertion1.cpp
@@ -35,6 +35,61 @@ void insertATHead(node *&head, int d)
     }
 }
 
+int length(node *head)
+{
+    int cnt = 0;
+    while (head != NULL)
+    {
+        cnt++;
+        head = head->next;
+    }
+    return cnt;
+}
+//(*n).next=head;
+//n->next=head;
+
+void insertInMiddle(node *&head, int d, int p)
+{
+    if (p == 0 || head == NULL)
+    {
+        insertATHead(head, d);
+        return;
+    }
+    else if (p == length(head))
+    {
+        insertATHead(head, d);
+    }
+    else
+    {
+        int jump = 1;
+        node *temp = head;
+        while (jump <= p - 1)
+        {
+            temp = temp->next;
+            jump++;
+        }
+        node *n = new node(d);
+        n->next = temp->next;
+        temp->next = n;
+    }
+}
+
+void insertAtTail(node *&head, int d)
+{
+    if (head == NULL)
+    {
+        head = new node(d);
+        return;
+    }
+    node *tail = head;
+    while (tail->next != NULL)
+    {
+        tail = tail->next;
+    }
+    tail->next = new node(d);
+    return;
+}
+
 void printNode(node *head)
 {
     while (head != NULL)
@@ -47,11 +102,17 @@ void printNode(node *head)
 int main()
 {
     node *head = NULL;
-    insertATHead(head, 3);
+    insertATHead(head, 4);
+    insertAtTail(head, 8);
+    insertInMiddle(head, 3, 2);
+
     insertATHead(head, 2);
 
     insertATHead(head, 1);
 
     insertATHead(head, 0);
+    insertInMiddle(head, 3, 3);
+    insertAtTail(head, 7);
+    insertAtTail(head, 8);
     printNode(head);
 }
